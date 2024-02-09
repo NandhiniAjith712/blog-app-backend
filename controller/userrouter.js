e001b8e9e736379585e840a3bb91431aca2a76c9
NandhiniAjith712 <nandhu7246@gmail.com>
2024-02-09 14:05:00 +0530

    added

--- controller/userrouter.js
+++ controller/userrouter.js
@@ -1,17 +1,12 @@
 const express = require("express")
-
 const usermodel = require("../models/usermodel")
-
 const router = express.Router()
-
 const bcrypt = require("bcryptjs")
 
 hashPasswordGenerator = async(pass)=>{
     const salt = await bcrypt.genSalt(10)
     return bcrypt.hash(pass,salt)
 }
-
-
 router.post("/user_entry",async(req,res)=>{
 
     let{data} = {"data":req.body}
@@ -33,6 +28,33 @@ router.post("/user_entry",async(req,res)=>{
 router.get("/viewall",async(req,res)=>{
     let data = await usermodel.find()
     res.json(data)
+})
+
+router.post("/signin",async(req,res)=>{
+    
+    let input = req.body
+    let emailid = req.body.emailid
+    let data = await usermodel.findOne({"emailid":emailid})
+    if(!data){
+        return res.json({
+            status:"incorrect mailid"
+        })
+    }
+    console.log(data)
+    let dbpassword = data.password
+    let inputpassword = req.body.password
+    console.log(dbpassword)
+    console.log(inputpassword)
+    const match = await bcrypt.compare(dbpassword,inputpassword)
+    if(!match){
+        return res.json({
+            status:"invalid password"
+        })
+    }
+    res.json({
+        status:"success"
+    })
+
 })
 
 module.exports= router
