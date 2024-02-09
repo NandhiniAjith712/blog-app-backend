c01c657d6c3de8971d3d29d26a51c129eb1a509e
NandhiniAjith712 <nandhu7246@gmail.com>
2024-02-09 10:47:45 +0530

    branch added

--- controller/userrouter.js
+++ controller/userrouter.js
@@ -5,9 +5,11 @@ const usermodel = require("../models/usermodel")
 const router = express.Router()
 
 router.post("/user_entry",async(req,res)=>{
+
+    
     let data = req.body
-    let user = new usermodel(data)
-    let result = await user.save()
+    //let user = new usermodel(data)
+    //let result = await user.save()
     res.json({
         status:"success"
     })
